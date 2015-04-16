f839e0a5107359949e5a5590ea50fd1390c8f34d
BastienIstil <bastien.thonnat@yahoo.fr>
2015-04-16 15:33:37 +0200

    test

--- Site_Web/Site_Web/Controllers/HomeController.cs
+++ Site_Web/Site_Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Site_Web.Controllers
 {
@@ -23,7 +19,6 @@ namespace Site_Web.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
