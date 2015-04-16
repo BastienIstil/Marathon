6a8723dae5e1764ede8bd7205199036cac33037d
BastienIstil <bastien.thonnat@yahoo.fr>
2015-04-16 14:24:02 +0200

    First Commit

---
+++ Site_Web/Site_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Site_Web.Startup))]
+namespace Site_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
