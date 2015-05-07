407c1fdec345486b952cbf0346f7de9ebb399c95
BastienIstil <bastien.thonnat@yahoo.fr>
2015-05-07 16:20:39 +0200

    ajout classe email

---
+++ Site_Web/Site_Web/Class_Metier/Web_Common/Email.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace Site_Web.Class_Metier.Web_Common
+{
+    public class Email
+    {
+
+
+        public static void SendEmail()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("thonnat.bastien@gmail.com");
+                mail.To.Add("thonnat.bastien@gmail.com");
+                mail.Subject = "Test Mail - 1";
+
+                mail.IsBodyHtml = true;
+                string htmlBody;
+
+                htmlBody = "Write some HTML code here";
+
+                mail.Body = htmlBody;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("thonnat.bastien@gmail.com", "");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
