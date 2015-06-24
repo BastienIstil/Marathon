8a15be75fe1ba8f5c3fdff598a207c8e07777e04
BastienIstil <bastien.thonnat@yahoo.fr>
2015-06-24 14:17:49 +0200

    encore un correctif

--- Site_Web/Site_Web/Controllers/CoureursController.cs
+++ Site_Web/Site_Web/Controllers/CoureursController.cs
@@ -168,7 +168,6 @@ namespace Site_Web.Controllers
             courreur.INS_ID = idInscrit;
 
             ViewBag.FED_ID = new SelectList(db.FEDERATIONs, "FED_ID", "FED_NOM", courreur.FED_ID);
-            ViewBag.CLU_ID = new SelectList(db.CLUBs, "CLU_ID", "CLU_NOM", courreur.CLU_ID);
             ViewBag.CAT_ID = new SelectList(db.CATEGORIEs, "CAT_ID", "CAT_LIBELLE", courreur.CAT_ID);
             ViewBag.INS_ID = new SelectList(db.INSCRITs, "INS_ID", "INS_LOGIN", courreur.INS_ID);
             return View(courreur);
@@ -201,7 +200,6 @@ namespace Site_Web.Controllers
             } 
 
             ViewBag.FED_ID = new SelectList(db.FEDERATIONs, "FED_ID", "FED_NOM", cOUREUR.FED_ID);
-            ViewBag.CLU_ID = new SelectList(db.CLUBs, "CLU_ID", "CLU_NOM", cOUREUR.CLU_ID);
             ViewBag.CAT_ID = new SelectList(db.CATEGORIEs, "CAT_ID", "CAT_LIBELLE", cOUREUR.CAT_ID);
             ViewBag.INS_ID = new SelectList(db.INSCRITs, "INS_ID", "INS_LOGIN", cOUREUR.INS_ID);
 
