9a87297993322fa251f7647cc4224b41b17b543f
BastienIstil <bastien.thonnat@yahoo.fr>
2015-07-10 14:15:47 +0200

    Classe borne commenté

--- Site_Web/Site_Web/Controllers/BornesController.cs
+++ Site_Web/Site_Web/Controllers/BornesController.cs
@@ -10,18 +10,30 @@ using Site_Web.App_Data;
 
 namespace Site_Web.Controllers
 {
+    /// <summary>
+    /// Bornes controller est le controlleur gérant le CRUD et différente vues lié au model BORNE.
+    /// </summary>
     public class BornesController : Controller
     {
         private MarathonEntities db = new MarathonEntities();
 
-        // GET: Bornes
+        
+
+        /// <summary>
+        /// Index du CRUD des bornes.
+        /// </summary>
+        /// <returns> la view Index.</returns>
         public ActionResult Index()
         {
             var bORNEs = db.BORNEs.Include(b => b.T_R_COURSE_COR);
             return View(bORNEs.ToList());
         }
 
-        // GET: Bornes/Details/5
+        /// <summary>
+        /// Affiche le detail d'une borne
+        /// </summary>
+        /// <param name="id">L'id correspondant à la borne devant être détaillé.</param>
+        /// <returns>La view detail avec les informations de la borne fournie en paramètre.</returns>
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,16 +48,25 @@ namespace Site_Web.Controllers
             return View(bORNE);
         }
 
-        // GET: Bornes/Create
+        /// <summary>
+        /// Permet la création d'une borne.
+        /// </summary>
+        /// <returns>La view création (GET) qui, si la validation du formulaire est bonne.
+        /// créera une nouvelle borne.
+        /// </returns>
         public ActionResult Create()
         {
             ViewBag.COR_ID = new SelectList(db.COURSEs, "COR_ID", "COR_NOM");
             return View();
         }
 
-        // POST: Bornes/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Permet la création d'une borne en POST.
+        /// </summary>
+        /// <param name="bORNE">La borne correspondant au formulaire remplie par l'utilisateur.</param>
+        /// <returns>La view création (POST) si la validation est fausse.
+        /// La view index si la validation est bonne.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "BOR_ID,COR_ID,BOR_EMPLACEMENT")] BORNE bORNE)
@@ -61,7 +82,13 @@ namespace Site_Web.Controllers
             return View(bORNE);
         }
 
-        // GET: Bornes/Edit/5
+        /// <summary>
+        /// Permet la modification d'une borne en GET.
+        /// </summary>
+        /// <param name="id">L'id correspondant à la borne devant être modifié.</param>
+        /// <returns>La view de modification (GET) qui, si la validation du formulaire est bonne.
+        /// modifiera ladite borne.
+        /// </returns>
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -77,9 +104,13 @@ namespace Site_Web.Controllers
             return View(bORNE);
         }
 
-        // POST: Bornes/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Permet la modification d'une borne en POST.
+        /// </summary>
+        /// <param name="bORNE">La borne correspondant au formulaire remplie par l'utilisateur.</param>
+        /// <returns>La view de modification (POST) si la validation est fausse.
+        /// La view index si la validation est bonne.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "BOR_ID,COR_ID,BOR_EMPLACEMENT")] BORNE bORNE)
@@ -94,7 +125,12 @@ namespace Site_Web.Controllers
             return View(bORNE);
         }
 
-        // GET: Bornes/Delete/5
+        /// <summary>
+        /// Permet la suppression d'une borne en GET.
+        /// </summary>
+        /// <param name="id">L'id correspondant à la borne devant être supprimé.</param>
+        /// <returns>La view de confirmation de suppression(GET).
+        /// </returns>
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -109,7 +145,11 @@ namespace Site_Web.Controllers
             return View(bORNE);
         }
 
-        // POST: Bornes/Delete/5
+        /// <summary>
+        /// Permet la suppression d'une borne en POST.
+        /// </summary>
+        /// <param name="id">L'id correspondant à la borne devant être supprimé.</param>
+        /// <returns>La view index si la validation est bonne.</returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -120,6 +160,10 @@ namespace Site_Web.Controllers
             return RedirectToAction("Index");
         }
 
+        /// <summary>
+        /// Destructeur de l'objet
+        /// </summary>
+        /// <param name="disposing">Le paramètre disposing est un booléen qui indique si l'appel de la méthode provient d'une méthode Dispose (sa valeur est true) ou d'un Finalize (sa valeur est false).</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
